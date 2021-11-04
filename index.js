34aa1a5856deba79de202a13a009568af9016d2b
William Xuan <xuanwilliam8@gmail.com>
2021-11-03 21:46:21 -0700

    unstable version of CRUD API

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
 const express = require("express"); // takes care of importing the express package
 const bodyParser = require("body-parser");
 const router = express.Router();
-const PORT = 1234;
+const PORT = 1234; // node index.js (file) to start server
 const app = express(); //calls express fuinction which returns the express application
 
 //Here we are configuring express to use body-parser as middle-ware.
 app.use(bodyParser.urlencoded({ extended: false}));
 app.use(bodyParser.json())
+
+
 
 router.post('/handle',(request,response) => {
   //code to perform particular action.
@@ -20,6 +22,10 @@ app.use("/", router);
 app.get("/hello", (req, res) => { //First we have app.get("/hello", ...) -> tells the server that we want to be able to handle a GET request to the /hello endpoint.
   res.send("Hello world");        // req stores info for incoming requests from the client
 });                               // res stores the server's response functions
+
+app.get("/login", (req, res) => { //First we have app.get("/hello", ...) -> tells the server that we want to be able to handle a GET request to the /hello endpoint.
+  res.send(index.html);        // req stores info for incoming requests from the client
+});
 
 app.listen(PORT, () => { //tells your application to listen for any requests that come in on port 1234
  console.log(`Server is listening on port: ${PORT}`);// port is a specific address 
@@ -35,3 +41,6 @@ var password = req.body.password;
 console.log("User name = " +user_name+", pasword is "+password );
 res.end("yes");
 });
+
+
+
---
+++ server.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+// create express app
+const app = express();
+// Setup server port
+const port = process.env.PORT || 4000;
+// parse requests of content-type - application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: true }))
+// parse requests of content-type - application/json
+app.use(bodyParser.json()) 
+// Configuring the database
+const dbConfig = require('./config/db.config.js');
+const mongoose = require('mongoose');
+mongoose.Promise = global.Promise;
+// Connecting to the database
+mongoose.connect(dbConfig.url, {
+useNewUrlParser: true
+}).then(() => {
+  console.log("Successfully connected to the database");
+}).catch(err => {
+  console.log('Could not connect to the database.', err);
+  process.exit();
+});
+// define a root/default route
+app.get('/', (req, res) => {
+   res.json({"message": "Hello World"});
+});
+// listen for requests
+app.listen(port, () => {
+   console.log(`Node server is listening on port ${port}`);
+});
